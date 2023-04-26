a14a916e65fb2fc721d3507d5c0f8adb1b47f5a0
Dwima Basumatary <dwima1583@gmail.com>
2023-04-26 14:00:29 +0530

    map

--- public/scripts/showMap.js
+++ public/scripts/showMap.js
@@ -3,10 +3,14 @@ mapboxgl.accessToken = mapboxToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
+    center: campground.geometry.coordinates.length === 0? [-74.5, 40]: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 9, // starting zoom
 });
 
-// new mapboxgl.Marker()
-//     .setLngLat([-74.5, 50])
-//     .addTo(map)
+new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates.length === 0? [-74.5, 40]: campground.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({offset: 25})
+        .setHTML(`<h3>${campground.title}</h3> <p>${campground.location}</p>`)
+    )
+    .addTo(map)
