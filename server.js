2fc55f1e6b34327b2d914761dfc97e9aa2227556
Dwima Basumatary <dwima1583@gmail.com>
2023-04-30 18:47:05 +0530

    security

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
 const passport = require('./src/config/passport');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
 
 const ExpressError = require('./src/utils/ExpressError');
 const flashMsg = require('./src/middlewares/flash');
@@ -18,21 +20,67 @@ const {currentUser} = require('./src/middlewares/authMiddlewares');
 const app = express();
 
 mongoose.connect(process.env.DATABASE_URL);
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Connection error: '));
-db.once('open', ()=>console.log("Database connected"))
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.set('layout', 'layouts/layout');
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net/",
+  ];
+  const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net/",
+  ];
+  const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+  ];
+  const fontSrcUrls = ["https://res.cloudinary.com/dkyoa3ps4/"];
+  app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'"],
+        connectSrc: ["'self'", ...connectSrcUrls],
+        scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+        styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+        workerSrc: ["'self'", "blob:"],
+        objectSrc: [],
+        imgSrc: [
+          "'self'",
+          "blob:",
+          "data:",
+          "https://res.cloudinary.com/dkyoa3ps4/",
+          "https://images.unsplash.com",
+        ],
+        fontSrc: ["'self'", ...fontSrcUrls],
+      },
+    })
+  );
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(methodOverride('_method'));
 app.use(expressLayouts);
+app.use(mongoSanitize());
+
+
 
 const sessionConfig = {
+    name: 'session',
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
@@ -52,12 +100,6 @@ app.use(flash());
 app.use(flashMsg);
 app.use(currentUser);
 
-app.use((req, res, next)=>{
-    // console.log(req.session);
-    // console.log(req.user);
-    // console.log(req.originalUrl);
-    next();
-})
 
 app.use('/', require('./src/routes/index'));
 app.use('/', require('./src/routes/users'));
--- src/middlewares/validateSchema.js
+++ src/middlewares/validateSchema.js
@@ -1,14 +1,34 @@
-const Joi = require('joi');
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
+
 const ExpressError = require('../utils/ExpressError');
+
+const Joi = BaseJoi.extend((joi)=>( {
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label} must not include HTML!}'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers){
+                const clean = sanitizeHtml(value);
+                if(clean !== value) return helpers.error('string.escapeHTML', {value})
+                return clean;
+            }
+        }
+    }
+}));
+
 
 module.exports.validateCampground = (req, res, next)=>{
     const campgroundSchema = Joi.object({
         campground: Joi.object({
-            title: Joi.string().required(),
+            title: Joi.string().required().escapeHTML(),
             price: Joi.number().required().min(0),
             // image: Joi.string().required(),
-            location: Joi.string().required(),
-            description: Joi.string().required(),
+            location: Joi.string().required().escapeHTML(),
+            description: Joi.string().required().escapeHTML(),
         }).required(),
         deleteImages: Joi.array(),
     })
@@ -21,7 +41,7 @@ module.exports.validateReview = (req, res, next)=>{
     const reviewSchema = Joi.object({
         review: Joi.object({
             rating: Joi.number().min(1).max(5).required(),
-            body: Joi.string().required()
+            body: Joi.string().required().escapeHTML()
         }).required()
     })
    
